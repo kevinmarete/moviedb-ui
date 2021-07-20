119075aceb2d4cc39f14cbb53c8e42b31343ffb9
KevinMarete <kevomarete@gmail.com>
2021-07-20 21:22:27 +0300

    converted Movie component from function to class component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {useParams} from 'react-router-dom';
 
 // Config
 import {IMAGE_BASE_URL, POSTER_SIZE} from '../config';
+
+// API
+import API from '../API';
 
 // Components
 import Grid from './Grid';
@@ -12,40 +15,70 @@ import MovieInfo from './MovieInfo';
 import MovieInfoBar from './MovieInfoBar';
 import Actor from './Actor';
 
-// Hook
-import {UseMovieFetch} from '../hooks/useMovieFetch';
-
 // Image
 import NoImage from '../images/no_image.jpg';
 
-const Movie = () => {
-	const {movieId} = useParams();
-	const {state: movie, loading, error} = UseMovieFetch(movieId);
+class Movie extends Component {
+	state = {
+		movie: {},
+		loading: true,
+		error: false,
+	};
 
-	if (loading) return <Spinner />;
-	if (error) return <div>Something went wrong ...</div>;
+	fetchMovie = async () => {
+		const {movieId} = this.props.params;
 
-	return (
-		<>
-			<Breadcrumb movieTitle={movie.original_title} />
-			<MovieInfo movie={movie} />
-			<MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue} />
-			<Grid header='Actors'>
-				{movie.actors.map(actor => (
-					<Actor
-						key={actor.credit_id} 
-						name={actor.name}
-						character={actor.character}
-						imageUrl={
-							actor.profile_path
-							? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
-							: NoImage
-						}
-					/>
-				))}
-			</Grid>
-		</>
-	);
-};
+		try {
+			this.setState({loading: true, error: false});
 
-export default Movie;
+			const movie = await API.fetchMovie(movieId);
+			const credits = await API.fetchCredits(movieId);
+			// Get directors only
+			const directors = credits.crew.filter((member) => member.job === 'Director');
+
+			this.setState({
+				movie: {
+					...movie,
+					actors: credits.cast,
+					directors,
+				},
+				loading: false,
+			});
+		} catch (error) {
+			this.setState({error: true, loading: false});
+		}
+	};
+
+	componentDidMount() {
+		this.fetchMovie();
+	}
+
+	render() {
+		const {movie, loading, error} = this.state;
+
+		if (loading) return <Spinner />;
+		if (error) return <div>Something went wrong ...</div>;
+
+		return (
+			<>
+				<Breadcrumb movieTitle={movie.original_title} />
+				<MovieInfo movie={movie} />
+				<MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue} />
+				<Grid header="Actors">
+					{movie.actors.map((actor) => (
+						<Actor
+							key={actor.credit_id}
+							name={actor.name}
+							character={actor.character}
+							imageUrl={actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}` : NoImage}
+						/>
+					))}
+				</Grid>
+			</>
+		);
+	}
+}
+
+const MovieWithParams = (props) => <Movie {...props} params={useParams()} />;
+
+export default MovieWithParams;
