2a669ba3b64cad18989fbab62ed819271673886b
KevinMarete <kevomarete@gmail.com>
2021-07-20 21:22:34 +0300

    converted SearchBar component from function to class component

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 // Image
@@ -7,37 +7,42 @@ import searchIcon from '../../images/search-icon.svg';
 // Styles
 import {Wrapper, Content} from './SearchBar.styles';
 
-const SearchBar = ({setSearchTerm}) => {
-	const [state, setState] = useState('');
-	const initial = useRef(true);
+class SearchBar extends Component {
+	state = {value: ''};
+	timeout = null;
 
-	useEffect(() => {
-		if (initial.current) {
-			initial.current = false;
-			return;
+	componentDidUpdate(_prevProps, prevState) {
+		if (this.state.value !== prevState.value){
+			const { setSearchTerm } = this.props;
+
+			clearTimeout(this.timeout);
+
+			this.timeout = setTimeout(() => {
+				const {value} = this.state;
+				setSearchTerm(value);
+			}, 500);
 		}
+	}
 
-		const timer = setTimeout(() => {
-			setSearchTerm(state);
-		}, 500);
 
-		return () => clearTimeout(timer);
-	}, [setSearchTerm, state]);
+	render() {
+		const {value} = this.state.value;
 
-	return (
+		return (
 		<Wrapper>
 			<Content>
 				<img src={searchIcon} alt="search-icon" />
 				<input
 					type="text"
 					placeholder="Search Movie"
-					onChange={(event) => setState(event.currentTarget.value)}
-					value={state}
+					onChange={(event) => this.setState({value: event.currentTarget.value})}
+					value={value}
 				/>
 			</Content>
 		</Wrapper>
-	);
-};
+		);
+	}
+}
 
 SearchBar.propTypes = {
 	callback: PropTypes.func,
